6672726337a453ec36b162b4abdad774cb7b2f2b
Коля Калашников <frdvmn@mail.ru>
2024-10-16 15:04:49 +0300

    projects sections posts

--- index.php
+++ index.php
@@ -213,60 +213,41 @@
         <div class="projects-slider__special">
             <div class="glide__track" data-glide-el="track">
                 <ul class="glide__slides">
-                    <li class="glide__slide">
-                        <div class="project-card">
-                            <div class="project-card__content">
-                                <span class="project-card__special">Специальные проекты</span>
-                                <h2 class="project-card__title">Ближний Восток - Европа</h2>
-                                <p class="project-card__tags">Руководитель: Ярослав Берестов, Первый Вице-президент,
-                                    Член Совета Правления ЦБИ</p>
-                                <p class="project-card__description">Специальная программа "Ближний Восток - Европа" в
-                                    рамках Центра Ближневосточных исследований направлена на всестороннее изучение
-                                    исторических, политических, экономических и культурных взаимодействий стран Ближнего
-                                    Востока с европейскими государствами. Основная цель программы – анализ сложившихся и
-                                    развивающихся отношений между регионами на протяжении истории, а также оценка
-                                    текущих тенденций и вызовов в этих взаимоотношениях.</p>
-                                <button data-micromodal-trigger="modal-1" class="btn">Читать далее</button>
-                            </div>
-                            <img src="/assets/images/test.jpg" alt="Проект" class="project-card__image">
-                        </div>
-                    </li>
-                    <li class="glide__slide">
-                        <div class="project-card">
-                            <div class="project-card__content">
-                                <span class="project-card__special">Специальные проекты</span>
-                                <h2 class="project-card__title">Ближний Восток - Европа</h2>
-                                <p class="project-card__tags">Руководитель: Ярослав Берестов, Первый Вице-президент,
-                                    Член Совета Правления ЦБИ</p>
-                                <p class="project-card__description">Специальная программа "Ближний Восток - Европа" в
-                                    рамках Центра Ближневосточных исследований направлена на всестороннее изучение
-                                    исторических, политических, экономических и культурных взаимодействий стран Ближнего
-                                    Востока с европейскими государствами. Основная цель программы – анализ сложившихся и
-                                    развивающихся отношений между регионами на протяжении истории, а также оценка
-                                    текущих тенденций и вызовов в этих взаимоотношениях.</p>
-                                <button data-micromodal-trigger="modal-1" class="btn">Читать далее</button>
-                            </div>
-                            <img src="/assets/images/test.jpg" alt="Проект" class="project-card__image">
-                        </div>
-                    </li>
-                    <li class="glide__slide">
-                        <div class="project-card">
-                            <div class="project-card__content">
-                                <span class="project-card__special">Специальные проекты</span>
-                                <h2 class="project-card__title">Ближний Восток - Европа</h2>
-                                <p class="project-card__tags">Руководитель: Ярослав Берестов, Первый Вице-президент,
-                                    Член Совета Правления ЦБИ</p>
-                                <p class="project-card__description">Специальная программа "Ближний Восток - Европа" в
-                                    рамках Центра Ближневосточных исследований направлена на всестороннее изучение
-                                    исторических, политических, экономических и культурных взаимодействий стран Ближнего
-                                    Востока с европейскими государствами. Основная цель программы – анализ сложившихся и
-                                    развивающихся отношений между регионами на протяжении истории, а также оценка
-                                    текущих тенденций и вызовов в этих взаимоотношениях.</p>
-                                <button data-micromodal-trigger="modal-1" class="btn">Читать далее</button>
-                            </div>
-                            <img src="/assets/images/test.jpg" alt="Проект" class="project-card__image">
-                        </div>
-                    </li>
+                <?php
+                    global $post;
+
+                    $query = new WP_Query( [
+                        'post_type'        => 'projects',
+                    ] );
+
+                    if ( $query->have_posts() ) {
+                        while ( $query->have_posts() ) {
+                            $query->the_post();
+                            ?>
+                            <?php if (get_field('project-special')) : ?>
+                                <li class="glide__slide">
+                                    <div class="project-card">
+                                        <div class="project-card__content">
+                                            <span class="project-card__special">Специальные проекты</span>
+                                            <h2 class="project-card__title"><?= get_the_title(); ?></h2>
+                                            <p class="project-card__tags"><?= ( get_field('project-subtitle' ) ) ? esc_html( get_field('project-subtitle') ) : '';?></p>
+                                            <p class="project-card__description"> <?= ( get_field('project-excertp' ) ) ? esc_html( get_field('project-excertp') ) : '';?></p>
+                                            <button data-micromodal-trigger="modal-project-special-<?=get_the_ID()?>" class="btn">Читать далее</button>
+                                        </div>
+                                        <?php if (has_post_thumbnail()) : ?>
+                                            <img src="<?= get_the_post_thumbnail_url()?>" alt="Проект" class="project-card__image">
+                                        <?php endif;  ?>
+                                    </div>
+                                </li>
+                            <?php endif;  ?>
+                            <?php
+                        }
+                    } else {
+                        echo "Добавьте проект";
+                    }
+
+                    wp_reset_postdata(); // Сбрасываем $post
+                ?>
                 </ul>
             </div>
 
@@ -302,52 +283,40 @@
         <div class="projects-slider">
             <div class="glide__track" data-glide-el="track">
                 <ul class="glide__slides">
-                    <li class="glide__slide">
-                        <div class="project-card">
-                            <div class="project-card__content">
-                                <h2 class="project-card__title">Ближний Восток - Европа</h2>
-                                <p class="project-card__description">Специальная программа "Ближний Восток - Европа" в
-                                    рамках Центра Ближневосточных исследований направлена на всестороннее изучение
-                                    исторических, политических, экономических и культурных взаимодействий стран Ближнего
-                                    Востока с европейскими государствами. Основная цель программы – анализ сложившихся и
-                                    развивающихся отношений между регионами на протяжении истории, а также оценка
-                                    текущих тенденций и вызовов в этих взаимоотношениях.</p>
-                                <button data-micromodal-trigger="modal-1" class="btn">Читать далее</button>
-                            </div>
-                            <img src="/assets/images/test.jpg" alt="Проект" class="project-card__image">
-                        </div>
-                    </li>
-                    <li class="glide__slide">
-                        <div class="project-card">
-                            <div class="project-card__content">
-                                <h2 class="project-card__title">Ближний Восток - Европа</h2>
-                                <p class="project-card__description">Специальная программа "Ближний Восток - Европа" в
-                                    рамках Центра Ближневосточных исследований направлена на всестороннее изучение
-                                    исторических, политических, экономических и культурных взаимодействий стран Ближнего
-                                    Востока с европейскими государствами. Основная цель программы – анализ сложившихся и
-                                    развивающихся отношений между регионами на протяжении истории, а также оценка
-                                    текущих тенденций и вызовов в этих взаимоотношениях.</p>
-                                <button data-micromodal-trigger="modal-1" class="btn">Читать далее</button>
-                            </div>
-                            <img src="/assets/images/test.jpg" alt="Проект" class="project-card__image">
-                        </div>
-                    </li>
-                    <li class="glide__slide">
-                        <div class="project-card">
-                            <div class="project-card__content">
-                                <h2 class="project-card__title">Ближний Восток - Европа</h2>
+                    <?php
+                    global $post;
 
-                                <p class="project-card__description">Специальная программа "Ближний Восток - Европа" в
-                                    рамках Центра Ближневосточных исследований направлена на всестороннее изучение
-                                    исторических, политических, экономических и культурных взаимодействий стран Ближнего
-                                    Востока с европейскими государствами. Основная цель программы – анализ сложившихся и
-                                    развивающихся отношений между регионами на протяжении истории, а также оценка
-                                    текущих тенденций и вызовов в этих взаимоотношениях.</p>
-                                <button data-micromodal-trigger="modal-1" class="btn">Читать далее</button>
-                            </div>
-                            <img src="/assets/images/test.jpg" alt="Проект" class="project-card__image">
-                        </div>
-                    </li>
+                    $query = new WP_Query( [
+                        'post_type'        => 'projects',
+                    ] );
+
+                    if ( $query->have_posts() ) {
+                        while ( $query->have_posts() ) {
+                            $query->the_post();
+                            ?>
+                            <?php if (get_field('project-special') !== true) : ?>
+                                <li class="glide__slide">
+                                    <div class="project-card">
+                                        <div class="project-card__content">
+                                            <h2 class="project-card__title"><?= get_the_title(); ?></h2>
+                                            <p class="project-card__tags"><?= ( get_field('project-subtitle' ) ) ? esc_html( get_field('project-subtitle') ) : '';?></p>
+                                            <p class="project-card__description"> <?= ( get_field('project-excertp' ) ) ? esc_html( get_field('project-excertp') ) : '';?></p>
+                                            <button data-micromodal-trigger="modal-project-special-<?=get_the_ID()?>" class="btn">Читать далее</button>
+                                        </div>
+                                        <?php if (has_post_thumbnail()) : ?>
+                                            <img src="<?= get_the_post_thumbnail_url()?>" alt="Проект" class="project-card__image">
+                                        <?php endif;  ?>
+                                    </div>
+                                </li>
+                            <?php endif;  ?>
+                            <?php
+                        }
+                    } else {
+                        echo "Добавьте проект";
+                    }
+
+                    wp_reset_postdata(); // Сбрасываем $post
+                ?>
                 </ul>
             </div>
 
@@ -470,7 +439,7 @@
         <p class="footer__email"><a href="mailto:cmesrussia@yandex.ru">Задавайте вопросы по электронной почте:
                 cmesrussia@yandex.ru</a></p>
         <div class="footer-icons">
-            <a href="https://t.me/cmesrussia" class="icons__vk" target="_blank">
+            <a href="https://vk.com/cmesrussia" class="icons__vk" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50"
                     fill="white">
                     <path
@@ -478,7 +447,7 @@
                     </path>
                 </svg>
             </a>
-            <a href="https://vk.com/cmesrussia" class="icons__vk" target="_blank">
+            <a href="https://t.me/cmesrussia" class="icons__vk" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50"
                     fill="white">
                     <path
@@ -554,6 +523,47 @@
     }
 
     wp_reset_postdata(); // Сбрасываем $post
+?>
+
+<?php
+global $post;
+
+$query = new WP_Query( [
+    'post_type'        => 'projects',
+] );
+
+if ( $query->have_posts() ) {
+    while ( $query->have_posts() ) {
+        $query->the_post();
+        ?>
+        <?php if (get_field('project-special')) : ?>
+            <div id="modal-project-special-<?= get_the_ID() ?>" class="modal" aria-hidden="true">
+                    <div class="modal-overlay" data-micromodal-close="data-micromodal-close"></div>
+                    <div class="modal-container" aria-modal="true" role="dialog" aria-labelledby="modal-2-title">
+                        <button class="btn-close">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 30 30" width="30px" height="30px"
+                                aria-label="Close modal" data-micromodal-close="data-micromodal-close">
+                                <path fill="white"
+                                    d="M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z" />
+                                </svg>
+                        </button>
+                        <div id="modal-1-content">
+                            <h2 class="modal__title"><?= get_the_title(); ?></h2>
+                            <p class="modal__tags">
+                                <?= ( get_field('project-subtitle' ) ) ? esc_html( get_field('project-subtitle') ) : '';?>
+                            </p>
+                            <p class="modal__content"><?= the_content() ?></p>
+                        </div>
+                    </div>
+                </div>
+        <?php endif;  ?>
+        <?php
+    }
+} else {
+    echo "Добавьте проект";
+}
+
+wp_reset_postdata(); // Сбрасываем $post
 ?>
 
 
