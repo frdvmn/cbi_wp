c5abb41979d19ea035f003c508e9e0453d4950d3
Kolya Kalashnikov <frdvmn@mail.ru>
2024-10-20 19:55:24 +0300

    add article image

--- home.php
+++ home.php
@@ -1,53 +1,79 @@
 <?php get_header(); ?>
 <section class="head-section__blog">
-    <h2 class="title">Основная миссия ЦБИ</h2>
+    <h2 class="title">СТАТЬИ</h2>
 </section>
 <div class="section articles-sidebar__container">
     <section class="articles">
         <article class="article">
-            <h2 class="article__title">Заголовок</h2>
-            <div class="article__categories">
-                <a href="" class="article__category">Ближний восток - центр</a>
+            <div class="article__content">
+                <h2 class="article__title">Заголовок</h2>
+                <div class="article__categories">
+                    <a href="" class="article__category">Ближний восток - центр</a>
+                </div>
+                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
+                <button class="btn">Читать далее</button>
             </div>
-            <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-            <button class="btn">Читать далее</button>
+            <div class="article__img--container">
+                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
+            </div>
         </article>
         <article class="article">
-            <h2 class="article__title">Заголовок</h2>
-            <div class="article__categories">
-                <a href="" class="article__category">Ближний восток - центр</a>
+            <div class="article__content">
+                <h2 class="article__title">Заголовок</h2>
+                <div class="article__categories">
+                    <a href="" class="article__category">Ближний восток - центр</a>
+                </div>
+                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
+                <button class="btn">Читать далее</button>
             </div>
-            <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-            <button class="btn">Читать далее</button>
+            <div class="article__img--container">
+                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
+            </div>
         </article>
         <article class="article">
-            <h2 class="article__title">Заголовок</h2>
-            <div class="article__categories">
-                <a href="" class="article__category">Ближний восток - центр</a>
+            <div class="article__content">
+                <h2 class="article__title">Заголовок</h2>
+                <div class="article__categories">
+                    <a href="" class="article__category">Ближний восток - центр</a>
+                </div>
+                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
+                <button class="btn">Читать далее</button>
             </div>
-            <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-            <button class="btn">Читать далее</button>
+            <div class="article__img--container">
+                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
+            </div>
         </article>
         <article class="article">
-            <h2 class="article__title">Заголовок</h2>
-            <div class="article__categories">
-                <a href="" class="article__category">Ближний восток - центр</a>
+            <div class="article__content">
+                <h2 class="article__title">Заголовок</h2>
+                <div class="article__categories">
+                    <a href="" class="article__category">Ближний восток - центр</a>
+                </div>
+                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
+                <button class="btn">Читать далее</button>
             </div>
-            <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-            <button class="btn">Читать далее</button>
+            <div class="article__img--container">
+                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
+            </div>
         </article>
         <article class="article">
-            <h2 class="article__title">Заголовок</h2>
-            <div class="article__categories">
-                <a href="" class="article__category">Ближний восток - центр</a>
+            <div class="article__content">
+                <h2 class="article__title">Заголовок</h2>
+                <div class="article__categories">
+                    <a href="" class="article__category">Ближний восток - центр</a>
+                </div>
+                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
+                <button class="btn">Читать далее</button>
             </div>
-            <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-            <button class="btn">Читать далее</button>
+            <div class="article__img--container">
+                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
+            </div>
         </article>
+
         <div class="pagination">
             <a href="" class="pagination__element">Назад</a>
             <a href="" class="pagination__element">1</a>
-            <a href="" class="pagination__element">2</a>
+            <a href="" class="pagination__element active">2</a>
             <a href="" class="pagination__element">3</a>
             <a href="" class="pagination__element">4</a>
             <a href="" class="pagination__element">Далее</a>
