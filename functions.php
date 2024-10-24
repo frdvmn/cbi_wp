48771e28d873e8c84b79b5b35fb8f09b5efc3b74
Kolya Kalashnikov <frdvmn@mail.ru>
2024-10-24 21:20:33 +0300

    add pagination

--- functions.php
+++ functions.php
@@ -154,7 +154,6 @@ add_action( 'admin_menu', 'true_remove_default_menus' );
  
 function true_remove_default_menus(){
  
-	remove_menu_page( 'edit.php' );                   // Записи
 
 	remove_menu_page( 'edit-comments.php' );          // Комментарии
 	remove_menu_page( 'themes.php' );                 // Внешний вид
@@ -163,4 +162,20 @@ function true_remove_default_menus(){
  
 }
 
+// удаляет H2 из шаблона пагинации
+add_filter('navigation_markup_template', 'my_navigation_template', 10, 2 );
+function my_navigation_template( $template, $class ){
+	/*
+	Вид базового шаблона:
+	<nav class="navigation %1$s" role="navigation">
+		<h2 class="screen-reader-text">%2$s</h2>
+		<div class="nav-links">%3$s</div>
+	</nav>
+	*/
 
+	return '
+	<nav class="%1$s" role="navigation">
+		<div class="nav-links">%3$s</div>
+	</nav>
+	';
+}
--- home.php
+++ home.php
@@ -7,80 +7,44 @@
 </section>
 <div class="section articles-sidebar__container">
     <section class="articles">
-        <article class="article">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<!-- Цикл WordPress -->
+    <article class="article">
             <div class="article__content">
-                <h2 class="article__title">Заголовок</h2>
+                <p class="article__time"><?php the_time('j F Y') ?></p>
+                <h2 class="article__title"><?php the_title();?></h2>
                 <div class="article__categories">
-                    <a href="" class="article__category">Ближний восток - центр</a>
+                    <?php 
+                        $categories = get_the_category();
+                        if( $categories ){
+                            foreach( $categories as $category ) {
+                                echo '<a class="article__category" href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>';
+                            }
+                        }
+                    ?>
                 </div>
-                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-                <button class="btn">Читать далее</button>
+                <p class="article__text"><?= get_the_excerpt();?></p>
+                <a class="btn" href="<?= get_the_permalink() ?>">Читать далее</a>
             </div>
             <div class="article__img--container">
-                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
+                <img src="<?= (get_the_post_thumbnail_url()) ? get_the_post_thumbnail_url() : get_template_directory_uri() . '/assets/images/news_img.jpeg' ?>" alt="" class="article__img">
             </div>
         </article>
-        <article class="article">
-            <div class="article__content">
-                <h2 class="article__title">Заголовок</h2>
-                <div class="article__categories">
-                    <a href="" class="article__category">Ближний восток - центр</a>
-                </div>
-                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-                <button class="btn">Читать далее</button>
-            </div>
-            <div class="article__img--container">
-                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
-            </div>
-        </article>
-        <article class="article">
-            <div class="article__content">
-                <h2 class="article__title">Заголовок</h2>
-                <div class="article__categories">
-                    <a href="" class="article__category">Ближний восток - центр</a>
-                </div>
-                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-                <button class="btn">Читать далее</button>
-            </div>
-            <div class="article__img--container">
-                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
-            </div>
-        </article>
-        <article class="article">
-            <div class="article__content">
-                <h2 class="article__title">Заголовок</h2>
-                <div class="article__categories">
-                    <a href="" class="article__category">Ближний восток - центр</a>
-                </div>
-                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-                <button class="btn">Читать далее</button>
-            </div>
-            <div class="article__img--container">
-                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
-            </div>
-        </article>
-        <article class="article">
-            <div class="article__content">
-                <h2 class="article__title">Заголовок</h2>
-                <div class="article__categories">
-                    <a href="" class="article__category">Ближний восток - центр</a>
-                </div>
-                <p class="article__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias labore ab odit nulla dolorem unde, ex similique, aperiam nobis sapiente nesciunt enim eum? Saepe obcaecati est debitis fugit cumque? Necessitatibus, delectus. Ullam quae perspiciatis maiores esse minus odit, recusandae iste temporibus dolorum fugiat quas atque quod ipsam, beatae voluptates vitae tempora dolorem quis necessitatibus! Quasi voluptatem recusandae voluptas eligendi laborum dolorum qui non reiciendis est corporis ullam libero veniam distinctio inventore obcaecati iure, repudiandae ab laboriosam quos! Necessitatibus quibusdam maiores repudiandae nemo molestiae incidunt asperiores illum fugiat ex, consequuntur similique repellendus cupiditate nesciunt facilis nulla mollitia quas, quos beatae iste!</p>
-                <button class="btn">Читать далее</button>
-            </div>
-            <div class="article__img--container">
-                <img src="<?= get_template_directory_uri() ?>/assets/images/news_img.jpeg" alt="" class="article__img">
-            </div>
-        </article>
-
-        <div class="pagination">
-            <a href="" class="pagination__element">Назад</a>
-            <a href="" class="pagination__element">1</a>
-            <a href="" class="pagination__element active">2</a>
-            <a href="" class="pagination__element">3</a>
-            <a href="" class="pagination__element">4</a>
-            <a href="" class="pagination__element">Далее</a>
-        </div>
+    <?php endwhile; else : ?>
+        <p>Записей нет.</p>
+    <?php endif; ?>
+        <?php the_posts_pagination([
+            'show_all'     => false, // показаны все страницы участвующие в пагинации
+            'end_size'     => 1,     // количество страниц на концах
+            'mid_size'     => 1,     // количество страниц вокруг текущей
+            'prev_next'    => true,  // выводить ли боковые ссылки "предыдущая/следующая страница".
+            'prev_text'    => __('« Назад'),
+            'next_text'    => __('Вперёд »'),
+            'add_args'     => false, // Массив аргументов (переменных запроса), которые нужно добавить к ссылкам.
+            'add_fragment' => '',     // Текст который добавиться ко всем ссылкам.
+            'screen_reader_text' => false,
+            'class'        => 'pagination', // CSS класс, добавлено в 5.5.0.
+        ]); ?>
     </section>
     <aside class="sidebar">
         <div class="sidebar-categories">
